31d242b7abd0a10c1dbf1b0efae5e43a9c3df8e1
Simon Jespersen <simjes91@me.com>
2016-06-08 20:20:27 +0200

    fixed log script

--- StandardConfig/getLogs.sh
+++ StandardConfig/getLogs.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 LOGFILE=/var/log/messages
-DIR=~/networkLogs/$(date + "%d-%m-%y_%T")
+DIR=~/networkLogs/$(date +"%d-%m-%y_%T")
 
 mkdir -p $DIR
-apt-get install -y sshpass
 
-sshpass -p "dhis" scp dhis@192.168.2.2:$LOGFILE $DIR/UniFiAP
-sshpass -p "dhis" scp dhis@192.168.2.5:$LOGFILE $DIR/UniFiDualAP
-sshpass -p "ubnt" scp ubnt@192.168.1.1:$LOGFILE $DIR/router
+scp dhis@192.168.2.2:$LOGFILE $DIR/UniFiAP
+scp dhis@192.168.2.5:$LOGFILE $DIR/UniFiDualAP
+scp ubnt@192.168.1.1:$LOGFILE $DIR/router
